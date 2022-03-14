8fcf62574086c355ef85a4e99f33731eaf941616
Norbert Gutpintér <norbert.gutpinter@adnovum.hu>
2022-03-14 16:25:21 +0100

    Initial Commit

---
+++ MVPArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPArchitecture
+//
+//  Created by Norbert Gutpintér on 2022. 03. 14..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
